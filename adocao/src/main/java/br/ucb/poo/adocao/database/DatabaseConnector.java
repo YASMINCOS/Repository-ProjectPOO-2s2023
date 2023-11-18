2bfedd32a73a0a2cc0b7145d3d971b60a23485ae
Yasmin Vitoria <yasmin.vitoria@digiteam.com.br>
2023-11-17 21:47:02 -0300

    first commit

--- adocao/src/main/java/br/ucb/poo/adocao/database/DatabaseConnector.java
+++ adocao/src/main/java/br/ucb/poo/adocao/database/DatabaseConnector.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnector {
     private static final String URL = "jdbc:mysql://localhost/adocao";
     private static final String USER = "root";
-    private static final String PASSWORD = "yasmin";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
